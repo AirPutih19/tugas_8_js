00efe2a6251085401d0df8899ffc162e21f242a0
AirPutih19 <kharismadwisj@gmail.com>
2023-05-13 17:16:55 +0700

    Tugas 8 JS
    Tugas 8 JS untuk niomic

---
+++ tugas8.js
@@ -0,0 +1,14 @@
+function jualbeliSepeda(){
+    var sepeda = {
+        type : "TDR-3000",
+        kondisi: "Bekas",
+        tahun: 2018,
+        harga: 3500000
+
+    }
+    sepeda.kondisi ="Baru";
+    sepeda.tahun = 2023;
+    sepeda.harga = 5000000;
+    console.log(sepeda)
+    
+}jualbeliSepeda()
